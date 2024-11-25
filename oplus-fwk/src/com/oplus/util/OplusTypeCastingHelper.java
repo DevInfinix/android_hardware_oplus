784c41de50b0d57db94e13c8ac13c51d0d36fd2a
DevInfinix <singhsamarjeet2007@gmail.com>
2024-11-25 10:46:03 +0530

    Revert "oplus-fwk: Implement minimal OplusTypeCastingHelper"
    This reverts commit f9e4c081a973cf176f218603d32dbc3baed6a548.

--- oplus-fwk/src/com/oplus/util/OplusTypeCastingHelper.java
+++
@@ -1,9 +0,0 @@
-package com.oplus.util;
-
-import android.content.Context;
-
-public final class OplusTypeCastingHelper {
-    public static <T> T typeCasting(Class<T> type, Context context) {
-        return null;
-    }
-}
